476074ed21d0ea1a34c9751be7d90903012ae722
kris <kris@Ubuntu-VB.(none)>
2012-11-25 14:09:32 -0600

    Add beforeSave to check if user has movie

--- Model/UserMovie.php
+++ Model/UserMovie.php
@@ -17,4 +17,29 @@ class UserMovie extends AppModel
                 'foreignKey' => 'user_id'
             )
         );
+    
+    /**
+     * Make sure user doesn't already have that movie
+     */
+    public function beforeSave(array $options = array())
+    {
+        $data = $this->data;
+        $user_id = $data['UserMovie']['user_id'];
+        $movie_id = $data['UserMovie']['movie_id'];
+        
+        $user_movie_ids = $this->userMovieIds($user_id);
+        
+        if(in_array($movie_id, $user_movie_ids)){
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public function userMovieIds($user_id)
+    {
+        return $this->find('list', array('conditions' => array('user_id' => $user_id),
+                                              'fields' => array('movie_id'),
+                                              'recursive' => -1));
+    }
 }

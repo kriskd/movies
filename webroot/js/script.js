59c75fe3592ecf350ab404412bd87f9af0d6be7d
kris <kris@Ubuntu-VB.(none)>
2013-02-13 19:59:15 -0600

    Disable submit during ajax

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -13,10 +13,12 @@ $(document).ready(function(){
     
     $('#MovieTitle').css('background', 'none')
         .ajaxStart(function(){
-            $(this).css('background', 'url("/img/5-1.gif") no-repeat center right');  
+            $(this).css('background', 'url("/img/5-1.gif") no-repeat center right');
+            $('input[type=submit]').attr('disabled', 'disabled');
         })
         .ajaxStop(function(){
-           $(this).css('background', 'none'); 
+           $(this).css('background', 'none');
+           $('input[type=submit]').removeAttr('disabled');
         })
         
     $('.submit input').click(function(){
